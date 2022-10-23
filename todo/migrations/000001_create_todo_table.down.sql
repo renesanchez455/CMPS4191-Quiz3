4b6d90a937ce4bfec36f1f025dacc69514ecb365
renesanchez455 <renesanchez455@yahoo.com>
2022-10-23 12:20:45 -0600

    Added database migrations

---
+++ todo/migrations/000001_create_todo_table.down.sql
@@ -0,0 +1,3 @@
+-- Filename: migrations/000001_create_todo_table.down.sql
+
+DROP TABLE IF EXISTS todo;
---
+++ todo/migrations/000001_create_todo_table.up.sql
@@ -0,0 +1,11 @@
+-- Filename: migrations/000001_create_todo_table.up.sql
+
+CREATE TABLE IF NOT EXISTS todo (
+    id bigserial PRIMARY KEY,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    name text NOT NULL,
+    details text NOT NULL,
+    priority text NOT NULL,
+    status text NOT NULL,
+    version integer NOT NULL DEFAULT 1
+);
