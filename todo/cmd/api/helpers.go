41fe11e83fb635717ab6f11f2a719ad83566a92f
renesanchez455 <renesanchez455@yahoo.com>
2022-10-23 10:23:20 -0600

    added code to handle bad requests and restrict requests

--- todo/cmd/api/helpers.go
+++ todo/cmd/api/helpers.go
@@ -5,8 +5,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -42,5 +45,61 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	w.WriteHeader(status)
 	// Write the []byte slice containing the JSON response body
 	w.Write(js)
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	// Use http.MaxBytesReader() to limit the size of the request body to
+	// 1 MB 2^20
+	maxBytes := 1_048_576
+	// Decode the request body into the target destination
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(dst)
+	// Check for a bad request
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		// Switch to check for the errors
+		switch {
+		// Check for syntax errors
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON(at character %d)", syntaxError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+		// Check for wrong types passed by the client
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+		// Empty body
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+			// Unmappable fields
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		// Too large
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+
+		// Pass  non-nil pointer error
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+			// default
+		default:
+			return err
+		}
+	}
+	// Call Decode() again
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
 	return nil
 }
--- todo/cmd/api/todo.go
+++ todo/cmd/api/todo.go
@@ -7,7 +7,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -25,9 +24,9 @@ func (app *application) createTodoHandler(w http.ResponseWriter, r *http.Request
 		Status   string `json:"status"`
 	}
 	// Initialize a new json.Decoder instance
-	err := json.NewDecoder(r.Body).Decode(&input)
+	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 	// Display the request
