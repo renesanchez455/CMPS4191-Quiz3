0780f8d99589518335a65344f6b0edbb4448c834
renesanchez455 <renesanchez455@yahoo.com>
2022-10-23 19:14:39 -0600

    Added ToDo form elm project

---
+++ todoform/src/Todo.elm
@@ -0,0 +1,51 @@
+module Todo exposing (Item)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+type alias Item =
+    { name : String
+    , details : String
+    , priority : String
+    , status: String
+    }
+
+initialModel : Item
+initialModel =
+    { name = ""
+    , details = ""
+    , priority = ""
+    , status = ""
+    }
+
+view : Item -> Html msg
+view item =
+    div [ ]
+        [ h1 [] [ text "ToDo" ]
+        , Html.form []
+            [ div []
+                [ text "Name"
+                , input [ id "name", type_ "text" ] []
+                ]
+            , div []
+                [ text "Details"
+                , input [ id "details", type_ "text" ] []
+                ]
+            , div []
+                [ text "Priority"
+                , input [ id "priority", type_ "text" ] []
+                ]
+            , div []
+                [ text "Status"
+                , input [ id "status", type_ "text" ] []
+                ]
+            , div []
+                [ button [ type_ "submit" ]
+                    [ text "Add to list" ]
+                ]
+            ]
+        ]
+
+main : Html msg
+main =
+    view initialModel
